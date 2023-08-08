ba346839b01f31c6d8e4fc09cff0a5d06cd927ff
Antonella Defilippi <antonelladefilipp14@gmail.com>
2023-08-08 15:16:26 -0300

    primera pre entrega del proyecto final 1

--- JS/app.js
+++ JS/app.js
@@ -1,161 +1,131 @@
 // Registrarme //
-
-let turno = "";
+// Función para obtener un dato válido (usuario, contraseña, etc.)
+function obtenerDato(mensaje) {
+  let dato;
+  do {
+    dato = prompt(mensaje);
+  } while (!dato);
+  return dato;
+}
+// Función para identificar al usuario
 function identificarDatos() {
-    const usuarioCorrecto = "Antonella";
-    let usuarioIngresado = prompt("Ingrese su usuario");
-
-    while (usuarioIngresado !== usuarioCorrecto) {
-        alert("El usuario ingresado no es correcto. Por favor ingréselo nuevamente.");
-        usuarioIngresado = prompt("Ingrese su usuario");
-    }
-
-    const contraseniaCorrecta = "contrasenia12";
-    let contraseniaIngresada = prompt("Ingrese su contraseña");
-
-    while (contraseniaIngresada !== contraseniaCorrecta) {
-        alert("La contraseña ingresada no es correcta. Por favor ingrésela nuevamente.");
-        contraseniaIngresada = prompt("Ingrese su contraseña");
-    }
-
-    // Bienvenidos //
-    alert("Inicio de sesión exitoso. ¡Bienvenido a mi turno online " + usuarioIngresado + "!");
+  const usuarioCorrecto = "Antonella";
+  const contraseniaCorrecta = "contrasenia12";
+  let usuarioIngresado = obtenerDato("Ingrese su usuario");
+  while (usuarioIngresado !== usuarioCorrecto) {
+    alert(
+      "El usuario ingresado no es correcto. Por favor ingréselo nuevamente."
+    );
+    usuarioIngresado = obtenerDato("Ingrese su usuario");
+  }
+  let contraseniaIngresada = obtenerDato("Ingrese su contraseña");
+  while (contraseniaIngresada !== contraseniaCorrecta) {
+    alert(
+      "La contraseña ingresada no es correcta. Por favor ingrésela nuevamente."
+    );
+    contraseniaIngresada = obtenerDato("Ingrese su contraseña");
+  }
+  alert(
+    "Inicio de sesión exitoso. ¡Bienvenido a mi turno online " +
+    usuarioIngresado +
+    "!"
+  );
 }
-
-identificarDatos();
-
 // Primera Eleccion
-let eleccion1;
-while (true) {
-    eleccion1 = prompt("Desea solicitar un turno para consulta? (si/no)").toLowerCase();
-    if (eleccion1 === "si" || eleccion1 === "no") {
-        break;
-    } else {
-        console.log(eleccion1);
-        alert("Respuesta inválida. Por favor, responda 'si' o 'no'.");
-    }
-}
-
-if (eleccion1 === "si") {
-    console.log(eleccion1);
+function primeraEleccion() {
+  const eleccion1 = prompt(
+    "Desea solicitar un turno para consulta? (si/no)"
+  ).toLowerCase();
+  if (eleccion1 === "si") {
     alert("Complete sus datos");
-    let apellido = prompt("Ingrese su Apellido y Nombre");
+    const apellido = obtenerDato("Ingrese su Apellido y Nombre");
     alert(`Turno: ${apellido}`);
-    let coberturaMedica = prompt("Ingrese cobertura médica");
+    const coberturaMedica = obtenerDato("Ingrese cobertura médica");
     alert(`Cobertura médica: ${coberturaMedica}`);
-    let numerotelefonico = prompt("Ingrese número de teléfono");
+    const numerotelefonico = obtenerDato("Ingrese número de teléfono");
     alert(`Número de teléfono: ${numerotelefonico}`);
     obtenerTurno();
-}
-
-// Segunda Eleccion
-else if (eleccion1 === "no") {
-    console.log(eleccion1);
+  } else if (eleccion1 === "no") {  // Segunda Eleccion 
     alert("Gracias por contactarse con B&B odontología. Lo esperamos pronto");
-}
-// primer parte selecciona el dia y horario del turno.
-function obtenerTurno() {
-    let diaSemana;
-    let horario;
-    let mensajeHorario;
-  
-    while (true) {
-      diaSemana = prompt(`Seleccione el día de la semana para el turno:
-        1 - Lunes
-        2 - Martes
-        3 - Miércoles
-        4 - Jueves
-        5 - Viernes`);
-  
-      switch (diaSemana) {
-        case "1":
-          mensajeHorario = "Horario disponibles:\n1 - 10:00hs";
-          break;
-        case "2":
-          mensajeHorario = "Horario disponibles:\n1 - 08:00hs";
-          break;
-        case "3":
-          mensajeHorario = "Horario disponibles:\n1 - 13:00hs";
-          break;
-        case "4":
-          mensajeHorario = "Horario disponibles:\n1 - 9:30hs";
-          break;
-        case "5":
-          mensajeHorario = "Horario disponibles:\n1 - 10:30hs";
-          break;
-        default:
-          alert("No ha elegido una opción válida. Por favor, seleccione un día válido (del 1 al 5).");
-          continue; // Reinicia el ciclo y muestra el prompt nuevamente.
-      }
-  
-      horario = prompt(mensajeHorario);
-  
-      switch (horario) {
-        case "1":
-          alert(`Turno reservado para el día lunes 10:00hs.`);
-          break;
-        case "2":
-          alert(`Turno reservado para el día martes 08:00hs.`);
-          break;
-        case "3":
-          alert(`Turno reservado para el día miércoles 13:00hs.`);
-          break;
-        case "4":
-          alert(`Turno reservado para el día jueves 9:30hs.`);
-          break;
-        case "5":
-          alert(`Turno reservado para el día viernes 10:30hs.`);
-          break;
-        default:
-          alert("No ha elegido una opción válida. Por favor, seleccione un horario válido (del 1 al 5).");
-      }
-  
-      // Sale del ciclo while si el usuario seleccionó un día y horario válidos.
-      break;
-    }
-  
-    // Parte 2: Elegir tratamientos y calcular el costo total
-    let totalCosto = 0;
-    while (true) {
-      const tratamientoSeleccionado = prompt(`Seleccione un tratamiento:
-        1 - Limpieza dental ($50)
-        2 - Extracción dental ($100)
-        3 - Relleno dental ($150)
-        0 - Terminar de agregar tratamientos`);
-  
-      const tratamientoNumero = parseInt(tratamientoSeleccionado);
-  
-      switch (tratamientoNumero) {
-        case 1:
-          totalCosto += 50;
-          alert("Tratamiento Limpieza dental agregado.");
-          break;
-        case 2:
-          totalCosto += 100;
-          alert("Tratamiento Extracción dental agregado.");
-          break;
-        case 3:
-          totalCosto += 150;
-          alert("Tratamiento Relleno dental agregado.");
-          break;
-        case 0:
-          // Si elige 0, sale del ciclo de selección de tratamientos.
-          break;
-        default:
-          alert("No ha elegido una opción válida para el tratamiento. Por favor, seleccione una opción válida (del 1 al 3 o 0 para terminar).");
-      }
-  
-      // Si elige 0, sale del ciclo de selección de tratamientos.
-      if (tratamientoNumero === 0) {
-        break;
-      }
-    }
-  
-    // Mostramos el resumen del costo
-    alert(`Costo Total:${totalCosto}`);
-  
-    alert("Gracias por contactarse con B&B odontología. Lo esperamos pronto.");
+  } else {
+    alert("Respuesta inválida. Por favor, responda 'si' o 'no'.");
+    primeraEleccion();
   }
-  
+}
+//selecciona el dia y horario del turno.
+function obtenerTurno() {
+  let diaSemana;
+  do {
+    const diaSeleccionado =
+      prompt(`Seleccione el día de la semana para el turno:
+      1 - Lunes
+      2 - Martes
+      3 - Miércoles
+      4 - Jueves
+      5 - Viernes`);
+    switch (diaSeleccionado) {
+      case "1":
+        diaSemana = "Lunes (10:00hs)";
+        break;
+      case "2":
+        diaSemana = "Martes (08:00hs)";
+        break;
+      case "3":
+        diaSemana = "Miércoles (13:00hs)";
+        break;
+      case "4":
+        diaSemana = "Jueves (9:30hs)";
+        break;
+      case "5":
+        diaSemana = "Viernes (10:30hs)";
+        break;
+      default:
+        alert(
+          "No ha elegido una opción válida. Por favor, seleccione un día válido (del 1 al 5)."
+        );
+        diaSemana = null;
+    }
+  } while (!diaSemana);
+  alert(`Turno reservado para el día ${diaSemana}.`);
+  // Elegir tratamientos y calcular el costo total
+  let totalCosto = 0;
+  let continuarAgregandoTratamientos = true;
+  while (continuarAgregandoTratamientos) {
+    const tratamientoSeleccionado = parseInt(
+      prompt(`Seleccione un tratamiento:
+      1 - Limpieza dental ($50)
+      2 - Extracción dental ($100)
+      3 - Relleno dental ($150)
+      0 - Terminar de agregar tratamientos`)
+    );
+    switch (tratamientoSeleccionado) {
+      case 0:
+        continuarAgregandoTratamientos = false;
+        break;
+      case 1:
+        totalCosto += 50;
+        alert(`Tratamiento agregado. Costo actual: $${totalCosto}`);
+        break;
+      case 2:
+        totalCosto += 100;
+        alert(`Tratamiento agregado. Costo actual: $${totalCosto}`);
+        break;
+      case 3:
+        totalCosto += 150;
+        alert(`Tratamiento agregado. Costo actual: $${totalCosto}`);
+        break;
+      default:
+        alert(
+          "No ha elegido una opción válida para el tratamiento. Por favor, seleccione una opción válida (del 1 al 3 o 0 para terminar)."
+        );
+    }
+  }
+  alert(`Costo Total: $${totalCosto}`);
+  alert("Gracias por contactarse con B&B odontología. Lo esperamos pronto.");
+}
+// Ejecutar las funciones
+identificarDatos();
+primeraEleccion();
 
-  
+
+
