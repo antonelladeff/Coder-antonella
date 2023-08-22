bb613fa56fce968e6e18793fb1867834d7a3bc3d
Antonella Defilippi <antonelladefilipp14@gmail.com>
2023-08-22 20:00:02 -0300

    segunda entrega

--- JS/segundaParte.js
+++ JS/segundaParte.js
@@ -8,7 +8,7 @@ class Paciente {
     this.telefono = telefono;
     this.turno = turno;
   }
-  
+
   // Método personalizado para mostrar información del paciente
   mostrarInfo() {
     return `Turno ${this.turno}: ${this.nombre} ${this.apellido}\nTeléfono: ${this.telefono}`;
@@ -23,19 +23,16 @@ let turnoActual = 0;
 function agregarPaciente() {
   // Incrementa el número de turno actual
   turnoActual++;
-  
-  // Pide al usuario que ingrese el nombre, apellido y número de teléfono del nuevo paciente
+
   const nombre = prompt("Ingrese el nombre del nuevo paciente:");
   const apellido = prompt("Ingrese el apellido del nuevo paciente:");
   const telefono = prompt("Ingrese el número de teléfono del nuevo paciente:");
-  
-  // Crea un nuevo objeto Paciente con los datos ingresados y el turno actual
+
   const paciente = new Paciente(nombre, apellido, telefono, turnoActual);
-  
+
   // Agrega el paciente al array de pacientes
   pacientes.push(paciente);
-  
-  // Muestra una alerta con el mensaje de que el paciente fue agregado
+
   alert(`Paciente ${nombre} ${apellido} agregado. Turno: ${paciente.turno}`);
 }
 
@@ -43,25 +40,24 @@ function agregarPaciente() {
 function mostrarListaPacientes() {
   // Crea una variable para almacenar la lista de pacientes como una cadena de texto
   let listaPacientes = "Lista de pacientes:\n";
-  
-  // Recorre el array de pacientes y agrega la información de cada paciente a la cadena
+
+
   for (const paciente of pacientes) {
     listaPacientes += paciente.mostrarInfo() + "\n";
   }
-  
+
   // Muestra una alerta con la lista de pacientes
   alert(listaPacientes);
 }
 
 // Función para buscar un paciente por número de turno
 function buscarPacientePorTurno() {
-  // Pide al usuario que ingrese el número de turno del paciente a buscar
   const turno = parseInt(prompt("Ingrese el número de turno del paciente a buscar:"));
-  
-  // Utiliza el método find() para buscar un paciente con el número de turno ingresado
+
+
   const pacienteEnTurno = pacientes.find(paciente => paciente.turno === turno);
-  
-  // Si encuentra un paciente con el número de turno, muestra su información
+
+
   if (pacienteEnTurno) {
     alert(`Paciente en turno ${turno}:\n${pacienteEnTurno.mostrarInfo()}`);
   } else {
@@ -72,12 +68,11 @@ function buscarPacientePorTurno() {
 
 // Función para filtrar pacientes por nombre
 function filtrarPacientesPorNombre() {
-  // Pide al usuario que ingrese el nombre para filtrar pacientes
   const nombre = prompt("Ingrese el nombre para filtrar pacientes:");
+
   
-  // Utiliza el método filter() para crear un nuevo array con pacientes cuyo nombre coincida
   const pacientesFiltrados = pacientes.filter(paciente => paciente.nombre === nombre);
-  
+
   // Si encuentra pacientes con el nombre ingresado, muestra su información
   if (pacientesFiltrados.length > 0) {
     let listaPacientes = `Pacientes con nombre ${nombre}:\n`;
@@ -86,7 +81,6 @@ function filtrarPacientesPorNombre() {
     }
     alert(listaPacientes);
   } else {
-    // Si no encuentra pacientes, muestra un mensaje indicando que no se encontraron pacientes con ese nombre
     alert(`No se encontraron pacientes con nombre ${nombre}`);
   }
 }
@@ -111,15 +105,15 @@ function agregarProductoAPaciente() {
   const pacienteIndex = parseInt(prompt("Ingrese el índice del paciente al que desea agregar un producto:"));
   mostrarProductos();
   const productoIndex = parseInt(prompt("Ingrese el índice del producto que desea agregar al paciente:"));
-  
+
   if (pacienteIndex >= 0 && pacienteIndex < pacientes.length && productoIndex >= 0 && productoIndex < productos.length) {
     const paciente = pacientes[pacienteIndex];
     const producto = productos[productoIndex];
-    
+
     if (!paciente.productos) {
       paciente.productos = [];
     }
-    
+
     paciente.productos.push(producto);
     alert(`Producto ${producto.nombre} agregado al paciente ${paciente.nombre} ${paciente.apellido}`);
   } else {
