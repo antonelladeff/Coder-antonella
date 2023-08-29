7e3615bb0efeac6ae0f6380d17c484d329a8a730
Antonella Defilippi <antonelladefilipp14@gmail.com>
2023-08-28 23:44:24 -0300

    modificando

--- JS/terceraParte.js
+++ JS/terceraParte.js
@@ -2,15 +2,8 @@
 const inicioFormulario = document.getElementById("inicio");
 
 // Función para mostrar errores en el formulario
-const mostrarMensaje = (text) => {
-	// Capturamos la etiqueta p donde mostraremos mensajes
-	const mensajes = document.getElementById("mensajes");
-	mensajes.textContent = text;
-	mensajes.classList.remove("hide");
-	setTimeout(() => {
-		mensajes.textContent = "";
-		mensajes.classList.add("hide");
-	}, 2000);
+const mostrarMensaje = (title, text, type) => {
+	Swal.fire(title, text, type);
 };
 
 // Agregamos el evento submit
@@ -21,19 +14,29 @@ inicioFormulario.addEventListener("submit", (e) => {
 	const nombre = document.getElementById("nombre").value;
 	// Capturamos el input password
 	const pass = document.getElementById("password").value;
-	// Buscamos si hay algún usuario que coincida con el nombre que ingresaron
-	const user = usuarios.find((user) => user.nombre === nombre);
-	// Si existe el usuario, revisamos su contraseña
-	if (user) {
-		// Si la contraseña es correcta, se redirecciona a la página que queramos
-		if (pass === user.password) {
-			location.href = "./index.html";
-		} else {
-			// Si la contraseña es incorrecta, mostramos un mensaje
-			mostrarMensaje("Contraseña incorrecta.");
-		}
-	} else {
-		// Si no existe usuario, mostramos un mensaje
-		mostrarMensaje("Nombre incorrecto.");
-	}
+	// Hacemos la petición de usuarios
+	fetch("./usuarios.json")
+		.then((response) => response.json())
+		.then((users) => {
+			// Buscamos si hay algún usuario que coincida con el nombre que ingresaron
+			const user = users.find((user) => user.nombre === nombre);
+			// Si existe el usuario, revisamos su contraseña
+			if (user) {
+				// Si la contraseña es correcta, se redirecciona a la página que queramos
+				if (pass === user.password) {
+					// Los datos son correctos, mostramos un mensaje
+					mostrarMensaje(`¡Buen día, ${user.nombre}!`, "Redireccionando...", "success");
+					// En un determinado tiempo lo redireccionamos
+					setTimeout(() => {
+						location.href = "../paginas/turnero.html";
+					}, 1500);
+				} else {
+					// Si la contraseña es incorrecta, mostramos un mensaje
+					mostrarMensaje("Error al iniciar sesión", "Contraseña incorrecta.", "error");
+				}
+			} else {
+				// Si no existe usuario, mostramos un mensaje
+				mostrarMensaje("Error al iniciar sesión", "Nombre incorrecto.", "error");
+			}
+		});
 });
---
+++ JS/usuarios.js
@@ -0,0 +1,14 @@
+const usuarios = [
+	{
+	nombre: "Antonella",
+	password: "123",
+	},
+	{
+	nombre: "Pedro",
+	password: "456",
+	},
+	{
+	nombre: "Ana",
+	password: "123abc",
+	},
+	]
