ad5382dc41ea00635de9bed137d65a784d6277a5
Antonella Defilippi <antonelladefilipp14@gmail.com>
2023-09-05 23:02:20 -0300

    tercer pre-entrega final

--- JS/registrarse.js
+++ JS/registrarse.js
@@ -11,7 +11,9 @@ registroForm.addEventListener("submit", (e) => {
     const contraseña = document.getElementById("contraseña").value;
     const confirmarContraseña = document.getElementById("confirmar_contraseña").value;
 
-    // Verificamos si el usuario ya existe
+    // Verificamos si el usuario ya existe en localStorage
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
     const usuarioExistente = usuarios.find((user) => user.nombre === nombre);
 
     if (usuarioExistente) {
@@ -22,28 +24,32 @@ registroForm.addEventListener("submit", (e) => {
             mostrarMensajeRegistro("Las contraseñas no coinciden.");
         } else {
             // Registramos al nuevo usuario
-            usuarios.push({
+            const nuevoUsuario = {
                 nombre: nombre,
                 correo: correo,
-                password: contraseña, 
-            });
-           
-            localStorage.setItem ('usuarios',JSON.stringify(usuarios));
-            
+                password: contraseña,
+            };
+
+            usuarios.push(nuevoUsuario);
+
+            // Actualizamos los usuarios en localStorage
+            localStorage.setItem('usuarios', JSON.stringify(usuarios));
             mostrarMensajeRegistro("Registro exitoso. Ahora puedes iniciar sesión.");
-            registroForm.reset(); // Limpiar el formulario
+            registroForm.reset(); // Limpiar el formulario después de un registro exitoso
 
             // Redirigir a la página de inicio de sesión después de un breve retraso
             setTimeout(() => {
-                window.location.href = "../index.html"; 
+                window.location.href = "../index.html";
             }, 2000); // 2000 milisegundos (2 segundos)
 
         }
     }
 });
 
-
+// Función para mostrar mensajes de registro
 function mostrarMensajeRegistro(mensaje) {
     const mensajeRegistro = document.getElementById("mensajeRegistro");
     mensajeRegistro.textContent = mensaje;
 }
+
+// Ahora, puedes utilizar el código de inicio de sesión anterior para manejar el inicio de sesión de usuarios
