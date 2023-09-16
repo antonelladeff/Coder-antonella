62550c44b67680c2c7d1d73b976be345832589e4
Antonella Defilippi <antonelladefilipp14@gmail.com>
2023-09-16 10:37:50 -0300

    otro ejemplo

--- JS/Turnos.js
+++ JS/Turnos.js
@@ -21,7 +21,7 @@
             });
         });
     
-        async function reservarTurno() {
+        function reservarTurno() {
             const nombre = document.getElementById("nombre").value;
             const apellido = document.getElementById("apellido").value;
             const obraSocial = document.getElementById("obraSocial").value;
@@ -37,59 +37,72 @@
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
                 confirmButtonText: 'Sí, reservar'
-            }).then(async (result) => {
+            }).then((result) => {
                 if (result.isConfirmed) {
-                    try {
-                        // Datos de reserva a enviar al servidor
-                        const reservaData = {
-                            nombre: nombre,
-                            apellido: apellido,
-                            obraSocial: obraSocial,
-                            telefono: telefono,
-                            horaSeleccionada: horaSeleccionada
-                        };
+                    // Datos de reserva a enviar al servidor
+                    const reservaData = {
+                        nombre: nombre,
+                        apellido: apellido,
+                        obraSocial: obraSocial,
+                        telefono: telefono,
+                        horaSeleccionada: horaSeleccionada
+                    };
         
-                        // Realizar la solicitud AJAX usando fetch y async/await
-                        const response = await fetch('/ruta/al/servidor', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify(reservaData)
-                        });
+                    // Realizar la solicitud AJAX usando fetch y promesas
+                    sendReservation('/ruta/al/servidor', reservaData)
+                        .then(data => {
+                            // Verificar la respuesta del servidor y mostrar un mensaje de éxito o error
+                            if (data.success) {
+                                Swal.fire(
+                                    '¡Reservado!',
+                                    'Tu turno ha sido reservado correctamente.',
+                                    'success'
+                                );
+                            } else {
+                                Swal.fire(
+                                    'Error',
+                                    'Hubo un problema al reservar el turno. Inténtalo de nuevo más tarde.',
+                                    'error'
+                                );
+                            }
         
-                        if (!response.ok) {
-                            throw new Error('Hubo un problema al reservar el turno.');
-                        }
-        
-                        const data = await response.json();
-        
-                        // Verificar la respuesta del servidor y mostrar un mensaje de éxito o error
-                        if (data.success) {
-                            Swal.fire(
-                                '¡Reservado!',
-                                'Tu turno ha sido reservado correctamente.',
-                                'success'
-                            );
-                        } else {
+                            // Cierra el modal después de la reserva
+                            $('#myModal').modal('hide');
+                        })
+                        .catch(error => {
+                            console.error('Error al realizar la solicitud AJAX:', error);
                             Swal.fire(
                                 'Error',
                                 'Hubo un problema al reservar el turno. Inténtalo de nuevo más tarde.',
                                 'error'
                             );
-                        }
-        
-                        // Cierra el modal después de la reserva
-                        $('#myModal').modal('hide');
-                    } catch (error) {
-                        console.error('Error al realizar la solicitud AJAX:', error);
-                        Swal.fire(
-                            'Error',
-                            'Hubo un problema al reservar el turno. Inténtalo de nuevo más tarde.',
-                            'error'
-                        );
-                    }
+                        });
                 }
             });
         }
         
+        function sendReservation(url, data) {
+            return new Promise((resolve, reject) => {
+                // Realizar la solicitud AJAX usando fetch
+                fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        reject(new Error('Hubo un problema al reservar el turno.'));
+                    }
+                    return response.json();
+                })
+                .then(data => resolve(data))
+                .catch(error => reject(error));
+            });
+        }
+        
+        
+
+       
+    
