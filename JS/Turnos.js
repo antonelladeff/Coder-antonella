02af6c2261f77dd06591c7e2c08423fedde9443b
Antonella Defilippi <antonelladefilipp14@gmail.com>
2023-09-17 11:05:01 -0300

    trabajando proyecto final

--- JS/Turnos.js
+++ JS/Turnos.js
@@ -14,59 +14,54 @@ horarioButtons.forEach(button => {
         const horario = this.textContent;
         const disponibilidad = this.parentNode.nextElementSibling.textContent;
 
-        // Verifica si el botón está disponible antes de mostrar el modal
-        if (disponibilidad === "Disponible") {
-            mostrarModal(horario);
-        }
-    });
+        // Muestra la ventana modal de confirmación usando Swal
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: `Deseas reservar un turno a las ${horaSeleccionada} para ${nombre} ${apellido} con obra social ${obraSocial} y teléfono ${telefono}?`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, reservar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                // Mostrar mensaje de éxito con Swal
+                Swal.fire(
+                    '¡Reservado!',
+                    'Tu turno ha sido reservado correctamente.',
+                    'success'
+                );
+            
+                // Cierra el modal después de la reserva
+                $('#myModal').modal('hide');
+            }
+            // Verifica si el botón está disponible antes de mostrar el modal
+            if (disponibilidad === "Disponible") {
+                // Guarda una referencia al botón seleccionado
+                const horarioSeleccionado = this;
+
+                // Muestra el modal y asigna el horario seleccionado
+                mostrarModal(horario);
+
+                // Asigna el horario seleccionado al elemento oculto en el modal
+                document.getElementById("horaSeleccionada").textContent = horario;
+
+
+                // Agrega un evento de clic al botón de reserva dentro del modal
+                document.getElementById("reservarBtnModal").addEventListener("click", function () {
+                    // Resto del código para reservar el turno
+
+                    // Deshabilita el botón de horario seleccionado
+                    horarioSeleccionado.setAttribute("disabled", "true");
+                    horarioSeleccionado.textContent = horarioSeleccionado.textContent + " (Reservado)";
+                });
+            }
+        });
+    })
+
+
 });
-
-// Función para reservar el turno con confirmación usando Swal
-function reservarTurno() {
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const obraSocial = document.getElementById("obraSocial").value;
-    const telefono = document.getElementById("telefono").value;
-    const horaSeleccionada = document.getElementById("horaSeleccionada").textContent;
-
-    // Muestra la ventana modal de confirmación usando Swal
-    Swal.fire({
-        title: '¿Estás seguro?',
-        text: `Deseas reservar un turno a las ${horaSeleccionada} para ${nombre} ${apellido} con obra social ${obraSocial} y teléfono ${telefono}?`,
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, reservar'
-    }).then((result) => {
-        if (result.isConfirmed) {
-
-            // Mostrar mensaje de éxito con Swal
-            Swal.fire(
-                '¡Reservado!',
-                'Tu turno ha sido reservado correctamente.',
-                'success'
-            );
-
-            // Cierra el modal después de la reserva
-            $('#myModal').modal('hide');
-        
-
-
-      // Deshabilitar el botón y cambiar la disponibilidad a "No Disponible"
-      const horarioSeleccionado = document.querySelector('.horario-btn.active');
-      if (horarioSeleccionado) {
-          horarioSeleccionado.disabled = true;
-          horarioSeleccionado.parentNode.nextElementSibling.textContent = 'No Disponible';
-      }
-  }
-});
-}
-
-
-
-
-
 
 
 
